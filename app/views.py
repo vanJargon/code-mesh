1c4f10b1893dcbcb9dba4808a0192d902bbb3ca4
vanJargon <vanessa_yun@mymail.sutd.edu.sg>
2016-11-07 09:38:28 +0800

    Correcting incorrect file in project directory

--- app/views.py
+++
@@ -1,9 +0,0 @@
-"""
-Routes and views for the flask application.
-"""
-from flask import render_template
-from FlaskWebProject import app
-
-@app.route('/')
-def main():
-    return render_template('index.html')
